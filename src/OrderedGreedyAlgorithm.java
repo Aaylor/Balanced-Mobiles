c4cc4763f4275003086b73723a74d8f29678c086
aaylor <loic.runarvot@gmail.com>
2014-12-02 15:34:58 +0100

    Algorithms: add abstract class to run timed algorithms

---
+++ src/OrderedGreedyAlgorithm.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class OrderedGreedyAlgorithm extends AbstractAlgorithms {
+
+    public OrderedGreedyAlgorithm(ArrayList<Integer> entry) {
+
+    }
+
+    public void mainFunction() {
+        System.out.println(this.getClass().getName());
+    }
+
+}
